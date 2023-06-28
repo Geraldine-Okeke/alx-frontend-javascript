9f18d46972b573cff13c764cf088bfd423a77b39
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-06-28 09:37:03 +0100

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,14 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Asynchronous code goes here
-    // For example, making an API request using fetch()
-    fetch('https://api.example.com')
-      .then(response => {
-        // Handle the response
-        resolve(response);
-      })
-      .catch(error => {
-        // Handle the error
-        reject(error);
-      });
+    // Perform asynchronous operations, such as API requests
+    // Example code with a setTimeout to simulate an asynchronous operation
+    setTimeout(() => {
+      const success = true; // Replace with your API response handling logic
+      if (success) {
+        resolve('API response'); // Resolve the Promise with the API response
+      } else {
+        reject(new Error('API request failed')); // Reject the Promise with an error
+      }
+    }, 1000); // Simulating a delay of 1 second
   });
 }
