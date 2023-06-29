fd158354c35fd7cf20325a66fa9e41f5a44547b5
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-06-29 18:41:59 +0100

    classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
+
