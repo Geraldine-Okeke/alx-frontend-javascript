32414dd399b4dc667bb7b3fe8a2b5dd87df27d36
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-06-29 14:49:35 +0100

    eslint

--- 0x02-ES6_classes/0-classroom.test.js
+++
@@ -1,9 +0,0 @@
-import ClassRoom from './0-classroom';
-
-describe('classroom', () => {
-  it('should have a maxStudentsSize of 10', () => {
-    expect.assertions(1);
-    const room = new ClassRoom(10);
-    expect(room._maxStudentsSize).toBe(10);
-  });
-});
