1f917389d35388ec6741b4fd5e267098b0f14da7
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-06-30 08:50:02 +0100

    let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const updatedTask = true;
+    const updatedTask2 = false;
+
+    return [updatedTask, updatedTask2];
   }
 
   return [task, task2];
