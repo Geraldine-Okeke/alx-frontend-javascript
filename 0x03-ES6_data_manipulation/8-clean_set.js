6657ba4fdc44bb0645b88f2370924b39174180ed
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-07-03 19:32:53 +0100

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-  return cleanedValues.join('-');
+  const valuesArray = Array.from(set);
+  const filteredValues = valuesArray.filter(value => value.startsWith(startString));
+  const cleanedValues = filteredValues.map(value => value.slice(startString.length));
+  return cleanedValues.join("-");
 }
 
 export default cleanSet;
